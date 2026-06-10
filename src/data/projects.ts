9c39004adf564cb79286121f83c77fe80fe422d4
Alekseypavlov14 <aleshapavlov9@gmail.com>
2026-06-10 19:09:16 +0300

    save progress

---
+++ src/data/projects.ts
@@ -0,0 +1,64 @@
+export interface Project {
+  slug: string
+  link: string
+
+  image: string
+  title: string
+  date: string
+  technologies: string
+  description: string
+}
+
+export const projects: Project[] = [
+  {
+    slug: 'data-xata',
+    link: 'https://data-xata.netlify.app',
+
+    image: '',
+    title: 'Data Xata - Cloud computing',
+    date: '20 April 2026',
+    technologies: 'React, TypeScript, Next JS',
+    description: `
+      A modern, responsive portfolio website showcasing creative and technical 
+      work with a clean, minimal design. It highlights projects, skills, 
+      and experience through interactive sections and smooth navigation. 
+      Built for performance and accessibility, it provides a seamless user 
+      experience across devices while reflecting a strong personal brand and 
+      professional identity.
+    `,
+  },
+  {
+    slug: 'data-xata-2',
+    link: 'https://data-xata.netlify.app',
+
+    image: '',
+    title: 'Data Xata - Cloud computing',
+    date: '20 April 2026',
+    technologies: 'React, TypeScript, Next JS',
+    description: `
+      A modern, responsive portfolio website showcasing creative and technical 
+      work with a clean, minimal design. It highlights projects, skills, 
+      and experience through interactive sections and smooth navigation. 
+      Built for performance and accessibility, it provides a seamless user 
+      experience across devices while reflecting a strong personal brand and 
+      professional identity.
+    `,
+  },
+  {
+    slug: 'data-xata-3',
+    link: 'https://data-xata.netlify.app',
+
+    image: '',
+    title: 'Data Xata - Cloud computing',
+    date: '20 April 2026',
+    technologies: 'React, TypeScript, Next JS',
+    description: `
+      A modern, responsive portfolio website showcasing creative and technical 
+      work with a clean, minimal design. It highlights projects, skills, 
+      and experience through interactive sections and smooth navigation. 
+      Built for performance and accessibility, it provides a seamless user 
+      experience across devices while reflecting a strong personal brand and 
+      professional identity.
+    `,
+  },
+]
